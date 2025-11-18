2130e88cba6a463640729559164b5f71af53295b
nyutdf2 <annslob@gmail.com>
2025-11-18 20:37:13 +0300

    minor fixes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
-import pprint
 import collections
 from dotenv import load_dotenv
 import configargparse
@@ -33,12 +32,14 @@ def main():
         '--path',
         help='Путь к файлу',
         env_var='DATA_FILE_PATH',
-        default='wine.xlsx')
+        default='wine.xlsx',
+    )
     data_file_path = parser.parse_args().path
 
     env = Environment(
         loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml']))
+        autoescape=select_autoescape(['html', 'xml']),
+    )
 
     template = env.get_template('template.html')
 
@@ -48,10 +49,11 @@ def main():
     years = current_year - company_foundation_year
 
     excel_wines_df = pandas.read_excel(
-        data_file_path,
+        io=data_file_path,
         sheet_name='Лист1',
         na_values=' ',
-        keep_default_na=False)
+        keep_default_na=False,
+    )
 
     wines = excel_wines_df.to_dict(orient='records')
 
@@ -59,12 +61,11 @@ def main():
     for wine in wines:
         wines_by_category[wine['Категория']].append(wine)
 
-    pprint.pprint(wines_by_category)
-
     rendered_page = template.render(
         wines_by_category=wines_by_category,
         company_age=years,
-        year_form=get_year_form(years))
+        year_form=get_year_form(years),
+    )
 
     with open('index.html', 'w', encoding='utf8') as file:
         file.write(rendered_page)

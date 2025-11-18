5bafc01d70bb29265d469baa689cc615df7985d0
nyutdf2 <annslob@gmail.com>
2025-11-18 20:26:57 +0300

    clear the code from indexes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import configargparse
 def get_year_form(years):
     last_digit = years % 10
     last_two_digits = years % 100
-    
+
     if 11 <= last_two_digits <= 14:
         return 'лет'
     elif last_digit == 1:
@@ -20,21 +20,22 @@ def get_year_form(years):
     elif 2 <= last_digit <= 4:
         return 'года'
     else:
-        return 'лет' 
-  
-      
+        return 'лет'
+
+
 def main():
     load_dotenv()
-    
-    parser = configargparse.ArgParser(description='Введите путь к файлу с данными')
+
+    parser = configargparse.ArgParser(
+        description='Введите путь к файлу с данными')
     parser.add(
         '-p',
-        '--path', 
-        help='Путь к файлу', 
-        env_var='DATA_FILE_PATH', 
+        '--path',
+        help='Путь к файлу',
+        env_var='DATA_FILE_PATH',
         default='wine.xlsx')
     data_file_path = parser.parse_args().path
-    
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml']))
@@ -51,19 +52,19 @@ def main():
         sheet_name='Лист1',
         na_values=' ',
         keep_default_na=False)
-    
+
     wines = excel_wines_df.to_dict(orient='records')
 
     wines_by_category = collections.defaultdict(list)
     for wine in wines:
         wines_by_category[wine['Категория']].append(wine)
-        
+
     pprint.pprint(wines_by_category)
 
     rendered_page = template.render(
         wines_by_category=wines_by_category,
         company_age=years,
-        year_form = get_year_form(years))
+        year_form=get_year_form(years))
 
     with open('index.html', 'w', encoding='utf8') as file:
         file.write(rendered_page)
